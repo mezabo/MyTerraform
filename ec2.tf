624703e93949d5080657ada7fcc9fda4b8b2619d
mezabo <106002876+mezabo@users.noreply.github.com>
2024-12-16 21:40:13 -0500

    removed all ec2 instances

--- ec2.tf
+++ ec2.tf
@@ -1,11 +1,11 @@
-resource "aws_instance" "web" {
-  ami           = "ami-0453ec754f44f9a4a"
-  instance_type = "t2.micro"
+# resource "aws_instance" "web" {
+#   ami           = "ami-0453ec754f44f9a4a"
+#   instance_type = "t2.micro"
 
-  tags = {
-    Name = "Helloglobe"
-  }
-}
+#   tags = {
+#     Name = "Helloglobe"
+#   }
+# }
 
 # data "aws_ami" "ubuntu" {
 #   most_recent = true
